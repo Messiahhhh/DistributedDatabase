a6aeabad4b881798f2467760f47aa9dff3d62b68
Gao Liang <gaoliang@ustc.edu>
2021-12-29 17:28:17 +0800

    添加“创建订单”、“查询订单”功能

--- e_marketSys/views.py
+++ e_marketSys/views.py
@@ -1,3 +1,6 @@
+from json.decoder import JSONDecoder
+from json.encoder import JSONEncoder
+from django import http
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,6 +9,8 @@ from .models import ProductDetailInfo,OrderInfo,OrderDetailInfo,UserBasicInfo
 from django.http import JsonResponse
 from django.http import HttpResponse
 from django.contrib.auth.hashers import make_password, check_password
+import datetime
+import json
 
 def dbtest(request):
     pass
@@ -62,6 +67,81 @@ def productDelete(request):
 def productList(request):
     pass
 def orderCreate(request):
-    pass
+    if request.method == 'POST':
+        dict = json.loads(request.body)
+        
+        orderid    = dict["_id"]        # 订单ID
+        userid     = dict["_userid"]
+        totalPrice = dict["totalPrice"]
+        products   = dict["product"]
+
+        userInfo = UserBasicInfo.objects(userName=userid)[0]
+
+        # 创建订单详情
+        OrderDetail=OrderDetailInfo(   orderId = orderid        ,
+                                        userId = userInfo       , 
+                                       payment = totalPrice     ).save()
+
+        # 生成物品清单 
+        productList = []
+        for item in products:
+            singleProduct = ProductDetailInfo.objects( proName = item["name"] )[0]
+            dict = {
+                 "name" : singleProduct.proName         ,
+                 "desc" : singleProduct.descript        ,
+                "price" : singleProduct.price           ,
+                "stock" : singleProduct.reserveCount-1  ,
+                  "img" : singleProduct.image           
+            }
+            # 更新库存
+            singleProduct.update ( dec__reserveCount = 1 )
+            productList.append(dict)
+
+        # 创建订单
+        itemCount = len(productList)
+        OrderInfo( orderId = OrderDetail , 
+                     count = itemCount   ,
+                     price = totalPrice  ,
+                     proId = productList ).save()
+        
+        expiredTime = OrderDetail.time + datetime.timedelta(days=1) - datetime.datetime.now()
+        expiredTime = expiredTime.total_seconds()
+        return JsonResponse ({   
+                                "status": 0,
+                                "data":{
+                                           "_id" : orderid           ,
+                                       "_userid" : userid            ,
+                                    "totalPrice" : totalPrice        ,
+                                        # 下单日期
+                                        "create" : OrderDetail.time  ,
+                                       # 还有1天过期 单位毫秒
+                                       "expired" : expiredTime       ,    
+                                      "products" : productList
+                                }
+                            })
+        # if
+
 def orderList(request):
-    pass
+    if request.method == 'GET':
+        orders = OrderDetailInfo.objects()
+        
+        # 生成订单列表
+        orderList = []
+        for item in orders:
+            expiredTime = item.time + datetime.timedelta(days=1) - datetime.datetime.now()
+            expiredTime = expiredTime.total_seconds()
+            dict = {
+                       "_id" : item.orderId     ,
+                   "_userid" : item.userId.id   ,
+                "totalPrice" : item.payment     ,
+                    "create" : item.time        ,
+                   "expired" : expiredTime           
+            }
+            orderList.append(dict)
+
+    return JsonResponse({
+                            "status": 0,
+                            "data": {
+                                "orders":orderList
+                            }
+                        })
